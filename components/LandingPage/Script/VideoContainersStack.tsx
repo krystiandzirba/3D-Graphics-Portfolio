2b8edda65d72231fad402feef4aa901658c23b3f
krystiandzirba <krystiandzirba8820@gmail.com>
2023-11-19 19:07:05 +0100

    v0.8.0 / replaced DOM elements with react

--- components/LandingPage/Script/VideoContainersStack.tsx
+++ components/LandingPage/Script/VideoContainersStack.tsx
@@ -13,6 +13,8 @@ export default function VideoContainersStack() {
   const [animation_state, set_animation_state] = useState(false);
   const [video_forward_time, set_video_forward_time] = useState(false);
   const [button_text, set_button_text] = useState("Portfolio");
+  const [remove_loading_page_content, set_remove_loading_page_content] = useState(false);
+  const [video_index, set_video_index] = useState({ forward: 30, reverse: 20, zoom: 10 });
 
   useEffect(() => {
     const portfolio_button_cover_timeout = setTimeout(() => {
@@ -36,112 +38,88 @@ export default function VideoContainersStack() {
   };
 
   function onClickAnimation(video: React.RefObject<HTMLVideoElement>) {
-    if (video.current && video_forward_time && !animation_state) {
-      const currentTime = video.current.currentTime;
-      if (currentTime === video.current.duration) {
-        set_animation_state(true);
-        playVideo(video_zoom_ref);
-        IndexPriority();
-        PortfolioButtonFadeOut();
-        PageFadeOut();
+    const checkAnimation = () => {
+      if (video.current && video_forward_time && !animation_state) {
+        const currentTime = video.current.currentTime;
+        if (currentTime === video.current.duration) {
+          set_animation_state(true);
+          playVideo(video_zoom_ref);
+          set_video_index({ forward: 10, reverse: 10, zoom: 30 });
+          // PageFadeOutWhite();
+          setTimeout(() => {
+            set_remove_loading_page_content(true);
+          }, 2300);
+        } else {
+          requestAnimationFrame(checkAnimation);
+        }
       }
-    }
+    };
 
-    return 0;
+    checkAnimation();
   }
 
   return (
     <>
-      <div className="page_fade_in"></div>
+      <div className="page_fade"></div>
       {portfolio_button_cover_state && <div className="portfolio_button_cover"></div>}
-      <button
-        onClick={() => {
-          set_portfolio_button_state(true);
-          setInterval(() => {
+      {!remove_loading_page_content && (
+        <button
+          onClick={() => {
+            set_portfolio_button_state(true);
             onClickAnimation(video_forward_ref);
-          }, 100);
-        }}
-        onMouseEnter={() => {
-          set_video_forward_time(true);
-          if (!portfolio_button_state) {
-            IndexChange(true);
-            playVideo(video_forward_ref);
-            pauseVideo(video_reverse_ref);
-            set_button_text("Explore");
-          }
-        }}
-        onMouseLeave={() => {
-          if (!portfolio_button_state) {
-            IndexChange(false);
-            playVideo(video_reverse_ref);
-            pauseVideo(video_forward_ref);
-            set_button_text("Portfolio");
-          }
-        }}
-        className="portfolio_button"
-        id="portfolio_button"
-      >
-        {button_text}
-      </button>
+          }}
+          onMouseEnter={() => {
+            set_video_forward_time(true);
+            if (!portfolio_button_state) {
+              set_video_index({ forward: 40, reverse: 10, zoom: 30 });
+              playVideo(video_forward_ref);
+              pauseVideo(video_reverse_ref);
+              set_button_text("Explore");
+            }
+          }}
+          onMouseLeave={() => {
+            if (!portfolio_button_state) {
+              set_video_index({ forward: 10, reverse: 40, zoom: 30 });
+              playVideo(video_reverse_ref);
+              pauseVideo(video_forward_ref);
+              set_button_text("Portfolio");
+            }
+          }}
+          className={!animation_state ? "portfolio_button" : "button_fade_out"}
+          id="portfolio_button"
+        >
+          {button_text}
+        </button>
+      )}
 
-      <div className="video_containers_stack">
-        <video muted preload="auto" ref={video_forward_ref} id="video_forward">
-          <source src={video_forward} type="video/mp4" />
-        </video>
-        <video muted preload="auto" ref={video_reverse_ref} id="video_reverse">
-          <source src={video_reverse} type="video/mp4" />
-        </video>
-        <video muted preload="auto" ref={video_zoom_ref} id="video_zoom">
-          <source src={video_zoom} type="video/mp4" />
-        </video>
-      </div>
+      {!remove_loading_page_content && (
+        <div className="video_containers_stack">
+          {/* prettier-ignore  */}
+          <video muted preload="auto" ref={video_forward_ref} id="video_forward" style={{ zIndex: video_index.forward }}>
+            <source src={video_forward} type="video/mp4" />
+          </video>
+          {/* prettier-ignore  */}
+          <video muted preload="auto" ref={video_reverse_ref} id="video_reverse" style={{ zIndex: video_index.reverse }}>
+            <source src={video_reverse} type="video/mp4" />
+          </video>
+          {/* prettier-ignore  */}
+          <video muted preload="auto" ref={video_zoom_ref} id="video_zoom" style={{ zIndex: video_index.zoom }}>
+            <source src={video_zoom} type="video/mp4" />
+          </video>
+        </div>
+      )}
+
+      <div className="test"></div>
     </>
   );
 }
 
-function IndexChange(mouse_state: boolean) {
-  const video_forward = document.getElementById("video_forward");
-  const video_reverse = document.getElementById("video_reverse");
-
-  if (mouse_state) {
-    if (video_forward !== null && video_reverse !== null) {
-      video_forward.style.zIndex = "4";
-      video_reverse.style.zIndex = "1";
-    }
-  }
-
-  if (!mouse_state) {
-    if (video_forward !== null && video_reverse !== null) {
-      video_reverse.style.zIndex = "4";
-      video_forward.style.zIndex = "1";
-    }
-  }
-}
-
-function IndexPriority() {
-  const video_forward = document.getElementById("video_forward");
-  const video_reverse = document.getElementById("video_reverse");
-  const video_zoom = document.getElementById("video_zoom");
-
-  if (video_forward !== null && video_reverse !== null && video_zoom !== null) {
-    video_forward.style.zIndex = "1";
-    video_reverse.style.zIndex = "1";
-    video_zoom.style.zIndex = "3";
-  }
-}
-
-function PortfolioButtonFadeOut() {
-  const portfolio_button = document.querySelector(".portfolio_button");
-  if (portfolio_button !== null) {
-    portfolio_button.classList.add("button_fade_out");
-  }
-}
-
-function PageFadeOut() {
+function PageFadeOutWhite() {
+  // console.log("1");
   setTimeout(function () {
-    const page_fade_out = document.querySelector(".page_fade_in");
+    const page_fade_out = document.querySelector(".page_fade");
     if (page_fade_out !== null) {
-      page_fade_out.classList.add("page_fade_out");
+      page_fade_out.classList.add("page_fade_out_white");
     }
   }, 1700);
 }
