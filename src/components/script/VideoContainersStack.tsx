cd582e9b5e63406a1ceafbde877b044d6e406649
krystiandzirba <krystiandzirba8820@gmail.com>
2023-11-22 21:43:58 +0100

    v0.14.0 / Added opacity switch for videos to reduce the transition flickering / removed portfolio button scale on hover to prevent the onMouse event to trigger multiple times / changed logic order to improve animation

--- src/components/script/VideoContainersStack.tsx
+++ src/components/script/VideoContainersStack.tsx
@@ -18,6 +18,7 @@ export default function VideoContainersStack() {
   const [button_text, set_button_text] = useState("Portfolio");
   const [remove_loading_page_content, set_remove_loading_page_content] = useState(false);
   const [video_index, set_video_index] = useState({ forward: 30, reverse: 20, zoom: 10 });
+  const [video_opacity, set_video_opacity] = useState({ forward: 1, reverse: 1 });
   const [load_portfolio_content, set_load_portfolio_content] = useState(false);
   const [page_loaded, set_page_loaded] = useState(false);
   const [video_forward_time, set_video_forward_time] = useState(0);
@@ -115,21 +116,24 @@ export default function VideoContainersStack() {
           onMouseEnter={() => {
             set_video_forward_state(true);
             if (!portfolio_button_state) {
-              set_button_text("Explore");
-
               video_forward_ref.current?.addEventListener("timeupdate", videoForwardTime);
               video_reverse_ref.current?.addEventListener("timeupdate", videoReverseTime);
-              playVideoMirror(video_forward_ref, video_reverse_time);
+              set_video_opacity({ forward: 1, reverse: 0 });
               set_video_index({ forward: 40, reverse: 10, zoom: 30 });
+              playVideoMirror(video_forward_ref, video_reverse_time);
+
+              set_button_text("Explore");
             }
           }}
           onMouseLeave={() => {
             if (!portfolio_button_state) {
-              set_button_text("Portfolio");
               video_forward_ref.current?.addEventListener("timeupdate", videoForwardTime);
               video_reverse_ref.current?.addEventListener("timeupdate", videoReverseTime);
-              playVideoMirror(video_reverse_ref, video_forward_time);
+              set_video_opacity({ forward: 0, reverse: 1 });
               set_video_index({ forward: 10, reverse: 40, zoom: 30 });
+              playVideoMirror(video_reverse_ref, video_forward_time);
+
+              set_button_text("Portfolio");
             }
           }}
           className={!animation_state ? "portfolio_button" : "button_fade_out"}
@@ -142,11 +146,11 @@ export default function VideoContainersStack() {
       {!remove_loading_page_content && (
         <div className="video_containers_stack">
           {/* prettier-ignore  */}
-          <video muted preload="auto" ref={video_forward_ref} id="video_forward" style={{ zIndex: video_index.forward }}>
+          <video muted preload="auto" ref={video_forward_ref} id="video_forward" style={{ zIndex: video_index.forward, opacity: video_opacity.forward }}>
             <source src={video_forward} type="video/mp4" />
           </video>
           {/* prettier-ignore  */}
-          <video muted preload="auto" ref={video_reverse_ref} id="video_reverse" style={{ zIndex: video_index.reverse }}>
+          <video muted preload="auto" ref={video_reverse_ref} id="video_reverse" style={{ zIndex: video_index.reverse, opacity: video_opacity.reverse }}>
             <source src={video_reverse} type="video/mp4" />
           </video>
           {/* prettier-ignore  */}
@@ -156,7 +160,7 @@ export default function VideoContainersStack() {
         </div>
       )}
 
-      <div className="app_version">v0.13.0</div>
+      <div className="app_version">v0.14.0</div>
 
       {load_portfolio_content && <div className="portfolio_background"></div>}
       {load_portfolio_content && <div className="test"></div>}
--- src/components/script/playVideoMirror.tsx
+++ src/components/script/playVideoMirror.tsx
@@ -1,5 +1,5 @@
 export const playVideoMirror = (videoRef: React.RefObject<HTMLVideoElement>, video_time: number) => {
-  if (videoRef.current && video_time != 0 && video_time < 1.873) {
+  if (videoRef.current && video_time != 0 && video_time < videoRef.current.duration) {
     videoRef.current.currentTime = videoRef.current.duration - video_time;
   }
   videoRef.current?.play();
